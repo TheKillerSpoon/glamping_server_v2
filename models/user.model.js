491bee5aea23beb2f091eb1de30cc24033f34546
mijam31 <156896989+mijam31@users.noreply.github.com>
2025-05-01 10:35:41 +0200

    User Model

---
+++ models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+// validering
+mongoose.set("runValidators", true);
+
+const productSchema = new Schema(
+  {
+    name: { type: stringify, required: true },
+    email: { type: String, unique: true, required: true },
+    Image: { type: String },
+    hashedPassword: { type: String, required: true },
+    role: { type: String, required: true, default: "user" },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.product || mongoose.model("user", productSchema);
