103f23ac828cc54bcd86be0925286db977f30463
silke-J <silk0776@mcdmstudent.dk>
2025-05-01 10:44:10 +0200

    image

--- models/user.model.js
+++ models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    Image: { type: String },
+    image: { type: String },
     hashedPassword: { type: String, required: true },
     role: { type: String, required: true, default: "user" },
   },
